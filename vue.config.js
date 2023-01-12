2a2ef27f513ea3771bec184d05b006e4a837f1df
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-01-12 03:58:32 +0300

    service-worker

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,32 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  pwa: {
+    name: "VTube - video player",
+    themeColor: "#000000",
+    workboxPluginMode: "GenerateSW",
+    manifestOptions: {
+      short_name: "VTube",
+      icons: [
+        {
+            src: "/favicon.ico",
+            type: "image/x-icon",
+            sized: "64x64 32x32 24x24 16x16",
+        },
+        {
+          src: "logo192.png",
+          type: "image/png",
+          sizes: "192x192",
+        },
+        {
+          src: "logo512.png",
+          type: "image/png",
+          sizes: "512x512",
+        },
+      ],
+      start_url: ".",
+      display: "standalone",
+      background_color: "#ffffff",
+    },
+  },
 });
